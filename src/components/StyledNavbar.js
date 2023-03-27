ddb87e83cca6923214b5f92d44cf82a4ffd67ef7
unknown <majestic13@gmail.com>
2023-03-27 16:54:12 +0300

    added styled navbar

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Login
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+/* import { NavLink } from "react-router-dom";
+
+<nav className="navbar">
+  <NavLink
+    to="/about"
+    style={({ isActive }) => {
+      return { color: isActive ? "red" : "grey" };
+    }}
+  >
+    Home
+  </NavLink>
+</nav>;
+ */
